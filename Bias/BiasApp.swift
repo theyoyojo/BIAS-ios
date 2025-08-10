af43c446e26ac499b1631e8ac09953183edb1a66
Joel Savitz <joel@underground.software>
2025-08-10 09:25:41 -0400

    Initial Commit

---
+++ Bias/BiasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiasApp.swift
+//  Bias
+//
+//  Created by Joel Savitz on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BiasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiasTests/BiasTests.swift
@@ -0,0 +1,17 @@
+//
+//  BiasTests.swift
+//  BiasTests
+//
+//  Created by Joel Savitz on 8/10/25.
+//
+
+import Testing
+@testable import Bias
+
+struct BiasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
